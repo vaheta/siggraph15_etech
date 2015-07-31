4f05e0fbce212288cf658470bc7db3a6e9d4a3bd
vaheta <vaheta@gmail.com>
2015-07-31 12:24:25 +0300

    added kinect code

---
+++ depth/CMakeFiles/depth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/depth.dir/openni_capture.cpp.o"
+  "depth.pdb"
+  "depth"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/depth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
